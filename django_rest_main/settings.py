ae5b40f6f99980e0d38e0b8b130ce56301f52dda
manmohanaeir1 <manmohanaeir001110@gmail.com>
2025-04-08 23:27:02 +0545

    for filters

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-     'blogs'
+     'blogs',
+     'django_filters'
 ]
 
 MIDDLEWARE = [
